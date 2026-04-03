7b323db9826e108b4867fd2258180502ba81e4d3
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2026-04-03 08:59:28 -0400

    kv-transfer: update 397b results

--- kv-transfer/run-qwen3.5-397b.sh
+++ kv-transfer/run-qwen3.5-397b.sh
@@ -6,9 +6,11 @@ REF_TAG="qwen3.5-397b-ud-q8_k_xl"
 
 TARGETS=(
     "ud-iq2_m:$HOME/projects/llms/qwen3.5-397b/UD-IQ2_M/Qwen3.5-397B-A17B-UD-IQ2_M-00001-of-00004.gguf"
+    "ud-iq3_xss:$HOME/projects/llms/qwen3.5-397b/UD-IQ3_XXS/Qwen3.5-397B-A17B-UD-IQ3_XXS-00001-of-00004.gguf"
+    "ud-iq4_xs:$HOME/projects/llms/qwen3.5-397b/UD-IQ4_XS/Qwen3.5-397B-A17B-UD-IQ4_XS-00001-of-00005.gguf"
 )
 
-N_PREDICT=512
+N_PREDICT=2048
 TEMP=0.6
 
 source "$(dirname "$0")/run.sh"
